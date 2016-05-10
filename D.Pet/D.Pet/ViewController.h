98b639e9478d81bd928cea159f1234ab093cf128
l k j y <yzj19932@sina.com>
2016-05-10 10:59:43 +0800

    asdc

---
+++ D.Pet/D.Pet/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  D.Pet
+//
+//  Created by l k j y on 16/5/10.
+//  Copyright © 2016年 尹照俊. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ D.Pet/D.Pet/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  D.Pet
+//
+//  Created by l k j y on 16/5/10.
+//  Copyright © 2016年 尹照俊. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"ghhh");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
